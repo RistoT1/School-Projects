61d972fa9f6e4faecb76c1baa8d024e6546fbb57
RISTO TOIVANEN <ristotoiv.rt@gmail.com>
2025-01-21 14:06:40 +0200

    Ristinolla

---
+++ Risti nolla/Risti nolla/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Risti_nolla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b0_0 = new System.Windows.Forms.Button();
+            this.b1_0 = new System.Windows.Forms.Button();
+            this.b2_0 = new System.Windows.Forms.Button();
+            this.b0_1 = new System.Windows.Forms.Button();
+            this.b1_1 = new System.Windows.Forms.Button();
+            this.b2_1 = new System.Windows.Forms.Button();
+            this.b1_2 = new System.Windows.Forms.Button();
+            this.b0_2 = new System.Windows.Forms.Button();
+            this.b2_2 = new System.Windows.Forms.Button();
+            this.btnNollaa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b0_0
+            // 
+            this.b0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b0_0.Location = new System.Drawing.Point(57, 53);
+            this.b0_0.Name = "b0_0";
+            this.b0_0.Size = new System.Drawing.Size(137, 113);
+            this.b0_0.TabIndex = 0;
+            this.b0_0.UseVisualStyleBackColor = true;
+            this.b0_0.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b1_0
+            // 
+            this.b1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b1_0.Location = new System.Drawing.Point(215, 53);
+            this.b1_0.Name = "b1_0";
+            this.b1_0.Size = new System.Drawing.Size(137, 113);
+            this.b1_0.TabIndex = 1;
+            this.b1_0.UseVisualStyleBackColor = true;
+            this.b1_0.Click += new System.EventHandler(this.b1_0_Click);
+            // 
+            // b2_0
+            // 
+            this.b2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b2_0.Location = new System.Drawing.Point(375, 53);
+            this.b2_0.Name = "b2_0";
+            this.b2_0.Size = new System.Drawing.Size(137, 113);
+            this.b2_0.TabIndex = 2;
+            this.b2_0.UseVisualStyleBackColor = true;
+            // 
+            // b0_1
+            // 
+            this.b0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b0_1.Location = new System.Drawing.Point(57, 172);
+            this.b0_1.Name = "b0_1";
+            this.b0_1.Size = new System.Drawing.Size(137, 113);
+            this.b0_1.TabIndex = 3;
+            this.b0_1.UseVisualStyleBackColor = true;
+            // 
+            // b1_1
+            // 
+            this.b1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b1_1.Location = new System.Drawing.Point(215, 172);
+            this.b1_1.Name = "b1_1";
+            this.b1_1.Size = new System.Drawing.Size(137, 113);
+            this.b1_1.TabIndex = 5;
+            this.b1_1.UseVisualStyleBackColor = true;
+            // 
+            // b2_1
+            // 
+            this.b2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b2_1.Location = new System.Drawing.Point(375, 172);
+            this.b2_1.Name = "b2_1";
+            this.b2_1.Size = new System.Drawing.Size(137, 113);
+            this.b2_1.TabIndex = 4;
+            this.b2_1.UseVisualStyleBackColor = true;
+            // 
+            // b1_2
+            // 
+            this.b1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b1_2.Location = new System.Drawing.Point(215, 302);
+            this.b1_2.Name = "b1_2";
+            this.b1_2.Size = new System.Drawing.Size(137, 113);
+            this.b1_2.TabIndex = 8;
+            this.b1_2.UseVisualStyleBackColor = true;
+            // 
+            // b0_2
+            // 
+            this.b0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b0_2.Location = new System.Drawing.Point(57, 302);
+            this.b0_2.Name = "b0_2";
+            this.b0_2.Size = new System.Drawing.Size(137, 113);
+            this.b0_2.TabIndex = 7;
+            this.b0_2.UseVisualStyleBackColor = true;
+            // 
+            // b2_2
+            // 
+            this.b2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.b2_2.Location = new System.Drawing.Point(375, 302);
+            this.b2_2.Name = "b2_2";
+            this.b2_2.Size = new System.Drawing.Size(137, 113);
+            this.b2_2.TabIndex = 6;
+            this.b2_2.UseVisualStyleBackColor = true;
+            // 
+            // btnNollaa
+            // 
+            this.btnNollaa.Location = new System.Drawing.Point(566, 53);
+            this.btnNollaa.Name = "btnNollaa";
+            this.btnNollaa.Size = new System.Drawing.Size(144, 55);
+            this.btnNollaa.TabIndex = 9;
+            this.btnNollaa.Text = "Nollaa";
+            this.btnNollaa.UseVisualStyleBackColor = true;
+            this.btnNollaa.Click += new System.EventHandler(this.btnNollaa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 472);
+            this.Controls.Add(this.btnNollaa);
+            this.Controls.Add(this.b1_2);
+            this.Controls.Add(this.b0_2);
+            this.Controls.Add(this.b2_2);
+            this.Controls.Add(this.b1_1);
+            this.Controls.Add(this.b2_1);
+            this.Controls.Add(this.b0_1);
+            this.Controls.Add(this.b2_0);
+            this.Controls.Add(this.b1_0);
+            this.Controls.Add(this.b0_0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b0_0;
+        private System.Windows.Forms.Button b1_0;
+        private System.Windows.Forms.Button b2_0;
+        private System.Windows.Forms.Button b0_1;
+        private System.Windows.Forms.Button b1_1;
+        private System.Windows.Forms.Button b2_1;
+        private System.Windows.Forms.Button b1_2;
+        private System.Windows.Forms.Button b0_2;
+        private System.Windows.Forms.Button b2_2;
+        private System.Windows.Forms.Button btnNollaa;
+    }
+}
+
---
+++ Risti nolla/Risti nolla/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Risti_nolla
+{
+    public partial class Form1 : Form
+    {
+        bool nextmarkX = true;
+        bool winner = false;
+        string[,] board = new string[3, 3];
+        int row = 0;
+        int col = 0;    
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Click += napit;
+                }
+                
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void napit(object sender, EventArgs e)
+        {
+            if (winner == false)
+            {
+                if (sender is Button button)
+                {
+                    if (string.IsNullOrEmpty(button.Text))
+                    {
+                        if (nextmarkX)
+                        {
+                            button.Text = "X";
+                            nextmarkX = !nextmarkX;
+                            Voitto();
+                        }
+                        else
+                        {
+                            button.Text += "O";
+                            nextmarkX = !nextmarkX;
+                            Voitto();
+                        }
+                    }
+                }
+            }
+        }
+        private void Voitto()
+        {
+            if (winner == false) 
+            {
+                if (b0_0.Text == "X" && b1_0.Text == "X" && b2_0.Text == "X" || b0_0.Text == "O" && b1_0.Text == "O" && b2_0.Text == "O")
+                {
+
+                    b0_0.BackColor = Color.Blue;
+                    b1_0.BackColor = Color.Blue;
+                    b2_0.BackColor = Color.Blue;
+                    winner = true;
+
+                }
+                else if (b0_1.Text == "X" && b1_1.Text == "X" && b2_1.Text == "X" || b0_1.Text == "O" && b1_1.Text == "O" && b2_1.Text == "O")
+                {
+
+                    b0_1.BackColor = Color.Blue;
+                    b1_1.BackColor = Color.Blue;
+                    b2_1.BackColor = Color.Blue;
+                    winner = true;
+                }
+                else if (b0_2.Text == "X" && b1_2.Text == "X" && b2_2.Text == "X" || b0_2.Text == "O" && b1_2.Text == "O" && b2_2.Text == "O")
+                {
+
+                    b0_2.BackColor = Color.Blue;
+                    b1_2.BackColor = Color.Blue;
+                    b2_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+
+                //Tarkistaa pystyrivit
+                else if (b0_0.Text == "X" && b0_1.Text == "X" && b0_2.Text == "X" || b0_0.Text == "O" && b0_1.Text == "O" && b0_2.Text == "O")
+                {
+
+                    b0_0.BackColor = Color.Blue;
+                    b0_1.BackColor = Color.Blue;
+                    b0_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+                else if (b1_0.Text == "X" && b1_1.Text == "X" && b1_2.Text == "X" || b1_0.Text == "O" && b1_1.Text == "O" && b1_2.Text == "O")
+                {
+
+                    b1_0.BackColor = Color.Blue;
+                    b1_1.BackColor = Color.Blue;
+                    b2_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+                else if (b2_0.Text == "X" && b2_1.Text == "X" && b1_2.Text == "X" || b2_0.Text == "O" && b2_1.Text == "O" && b2_2.Text == "O")
+                {
+
+                    b2_0.BackColor = Color.Blue;
+                    b2_1.BackColor = Color.Blue;
+                    b2_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+                //tarkistaa ristiin
+                else if (b0_0.Text == "X" && b1_1.Text == "X" && b2_2.Text == "X" || b0_0.Text == "O" && b1_1.Text == "O" && b2_2.Text == "O")
+                {
+
+                    b0_0.BackColor = Color.Blue;
+                    b1_1.BackColor = Color.Blue;
+                    b2_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+                else if (b2_0.Text == "X" && b1_1.Text == "X" && b0_2.Text == "X" || b2_0.Text == "O" && b1_1.Text == "O" && b0_2.Text == "O")
+                {
+
+                    b2_0.BackColor = Color.Blue;
+                    b1_1.BackColor = Color.Blue;
+                    b0_2.BackColor = Color.Blue;
+                    winner = true;
+                }
+            }
+        }   
+            
+
+        private void b1_0_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnNollaa_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Text = string.Empty;
+                    button.BackColor = Color.White;
+                }
+                winner = false;
+
+            }
+        }
+    }
+}
