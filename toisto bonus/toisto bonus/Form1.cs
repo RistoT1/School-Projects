e502de704cb4d19b3091247bf3478af03b26b9f2
RISTO TOIVANEN <ristotoiv.rt@gmail.com>
2025-01-21 11:34:26 +0200

    Toisto Tehtävä missä luku kertaantuu aina 2 kunnes 64

---
+++ toisto bonus/toisto bonus/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toisto_bonus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long luku = 1;
+
+            for (int i = 1; i <= 64; i++) 
+            {
+                textBox1.Text += "ruutu" + i.ToString() + ": " + luku.ToString() + Environment.NewLine;
+                luku *= 2;
+            }
+            
+               
+        }
+    }
+}
