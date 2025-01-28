361607be052fb02058afd7892aca6e9872624b7b
RISTO TOIVANEN <ristotoiv.rt@gmail.com>
2025-01-28 14:06:03 +0200

    Mäkihyppy

---
+++ Mäkihyppy teht/Mäkihyppy teht/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Mäkihyppy_teht
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Jatka = new System.Windows.Forms.Button();
+            this.txtKpiste = new System.Windows.Forms.TextBox();
+            this.KPisyteL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Hyppääjä = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hypynPituus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuomari1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuomari2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuomari3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuomari4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tuomari5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HypynPituusPisteet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tyylipisteet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KokonaisPisteet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hyppääjä,
+            this.hypynPituus,
+            this.Tuomari1,
+            this.Tuomari2,
+            this.Tuomari3,
+            this.Tuomari4,
+            this.Tuomari5,
+            this.HypynPituusPisteet,
+            this.Tyylipisteet,
+            this.KokonaisPisteet});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(2380, 264);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Jatka
+            // 
+            this.Jatka.Location = new System.Drawing.Point(1883, 361);
+            this.Jatka.Name = "Jatka";
+            this.Jatka.Size = new System.Drawing.Size(102, 49);
+            this.Jatka.TabIndex = 3;
+            this.Jatka.Text = "Jatka";
+            this.Jatka.UseVisualStyleBackColor = true;
+            this.Jatka.Click += new System.EventHandler(this.Jatka_Click);
+            // 
+            // txtKpiste
+            // 
+            this.txtKpiste.Location = new System.Drawing.Point(1745, 382);
+            this.txtKpiste.Name = "txtKpiste";
+            this.txtKpiste.Size = new System.Drawing.Size(100, 26);
+            this.txtKpiste.TabIndex = 4;
+            // 
+            // KPisyteL
+            // 
+            this.KPisyteL.AutoSize = true;
+            this.KPisyteL.Location = new System.Drawing.Point(1745, 361);
+            this.KPisyteL.Name = "KPisyteL";
+            this.KPisyteL.Size = new System.Drawing.Size(59, 20);
+            this.KPisyteL.TabIndex = 5;
+            this.KPisyteL.Text = "K-Piste";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1844, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "m";
+            // 
+            // Hyppääjä
+            // 
+            this.Hyppääjä.HeaderText = "hyppaaja";
+            this.Hyppääjä.MinimumWidth = 8;
+            this.Hyppääjä.Name = "Hyppääjä";
+            // 
+            // hypynPituus
+            // 
+            this.hypynPituus.HeaderText = "Hypyn Pituus";
+            this.hypynPituus.MinimumWidth = 8;
+            this.hypynPituus.Name = "hypynPituus";
+            this.hypynPituus.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Tuomari1
+            // 
+            this.Tuomari1.HeaderText = "Tuomari1";
+            this.Tuomari1.MinimumWidth = 8;
+            this.Tuomari1.Name = "Tuomari1";
+            // 
+            // Tuomari2
+            // 
+            this.Tuomari2.HeaderText = "Tuomari2";
+            this.Tuomari2.MinimumWidth = 8;
+            this.Tuomari2.Name = "Tuomari2";
+            // 
+            // Tuomari3
+            // 
+            this.Tuomari3.HeaderText = "Tuomari3";
+            this.Tuomari3.MinimumWidth = 8;
+            this.Tuomari3.Name = "Tuomari3";
+            // 
+            // Tuomari4
+            // 
+            this.Tuomari4.HeaderText = "Tuomari4";
+            this.Tuomari4.MinimumWidth = 8;
+            this.Tuomari4.Name = "Tuomari4";
+            // 
+            // Tuomari5
+            // 
+            this.Tuomari5.HeaderText = "Tuomari5";
+            this.Tuomari5.MinimumWidth = 8;
+            this.Tuomari5.Name = "Tuomari5";
+            // 
+            // HypynPituusPisteet
+            // 
+            this.HypynPituusPisteet.HeaderText = "Hypynpituus pisteet";
+            this.HypynPituusPisteet.MinimumWidth = 8;
+            this.HypynPituusPisteet.Name = "HypynPituusPisteet";
+            // 
+            // Tyylipisteet
+            // 
+            this.Tyylipisteet.HeaderText = "Tyylipisteet";
+            this.Tyylipisteet.MinimumWidth = 8;
+            this.Tyylipisteet.Name = "Tyylipisteet";
+            this.Tyylipisteet.ReadOnly = true;
+            // 
+            // KokonaisPisteet
+            // 
+            this.KokonaisPisteet.HeaderText = "KokonaisPisteet";
+            this.KokonaisPisteet.MinimumWidth = 8;
+            this.KokonaisPisteet.Name = "KokonaisPisteet";
+            this.KokonaisPisteet.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2654, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KPisyteL);
+            this.Controls.Add(this.txtKpiste);
+            this.Controls.Add(this.Jatka);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Jatka;
+        private System.Windows.Forms.TextBox txtKpiste;
+        private System.Windows.Forms.Label KPisyteL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hyppääjä;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hypynPituus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuomari1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuomari2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuomari3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuomari4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tuomari5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HypynPituusPisteet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tyylipisteet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KokonaisPisteet;
+    }
+}
+
---
+++ Mäkihyppy teht/Mäkihyppy teht/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mäkihyppy_teht
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Jatka_Click(object sender, EventArgs e)
+        {
+            //listat
+            List<double> TuomariPisteet = new List<double>(); // Lista Tuomarien antamista pisteistä
+
+            //perusarvot
+            double Kpiste = 0; // Kpiste
+            double hypynPituus = 0; //hypyn pituus
+            
+            
+            //tuomarien luvut eri muodoissa
+            double TuomariParsed;   // tuomarin luku kun parsetettu
+            double RoundedTuomari;  // pyötistetty tuomarin pisteet 0,5
+
+            //pisteet
+            double KokonaisPisteet = 0; //Kokonaispisteet
+            double tyyliPisteet = 0;    // tuomarien antamat pisteet
+            double hypynPituuspisteet = 0; // hypyn pituudesta saadut pisteet
+
+            try
+            {
+                Kpiste = int.Parse(txtKpiste.Text); // parsettaa K pisteen
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("syötä luku K-pisteeseen"); //virhe ilmotus
+            }
+
+            for (int rivi = 0; rivi < dataGridView1.RowCount; rivi++)   //joka rivin lukeminen
+            {
+                KokonaisPisteet = 0;            // nollaus
+                tyyliPisteet = 0;               
+                hypynPituuspisteet = 0;         
+                TuomariPisteet.Clear();
+                for (int solu = 2; solu <= 6; solu++) //joka tuomari solun lukeminen
+                {
+                    //jos tuomari solu ei ole tyhjä ja parsettaa
+                    if(dataGridView1.Rows[rivi].Cells[solu].Value != null && double.TryParse(dataGridView1.Rows[rivi].Cells[solu].Value.ToString(),out TuomariParsed)) 
+                    {
+                        RoundedTuomari = Math.Round(TuomariParsed * 2, 0, MidpointRounding.AwayFromZero) / 2; // pyöristää luvun lähimpään 0,5
+                        TuomariPisteet.Add(RoundedTuomari); // lisää pyöristetyn tuomari luvun pisteisiin
+                    }    
+                }
+                //jos tuomari solu ei ole tyhjä ja parsettaa
+                if (dataGridView1.Rows[rivi].Cells[1].Value != null && double.TryParse(dataGridView1.Rows[rivi].Cells[1].Value.ToString(), out hypynPituus)) 
+                {    
+                        if (hypynPituus >= Kpiste) //jos hypyn pituus on pitempi kuin K-piste, lisää hypynpituus pisteisiin 60 + yli metrit * 1,8 
+                        {
+                            hypynPituuspisteet = 60 + ((hypynPituus - Kpiste) * 1.8);
+                        }
+                        else if (hypynPituus < Kpiste) // jos alle K-pisteen vähentää 60 pisteestä vajaat metrit.
+                        {
+                            hypynPituus = 60 - (Kpiste - hypynPituus) * 1.8;
+                        }     
+                }
+
+                if (TuomariPisteet.Count >= 3)  // jos tuomareita on 3 tai yli niin suorittaa laskennan
+                {
+                    TuomariPisteet.Remove(TuomariPisteet.Max());    //poistaa Max
+                    TuomariPisteet.Remove(TuomariPisteet.Min());    //poistaa Min
+                    tyyliPisteet = TuomariPisteet.Sum();            // laskee yhteen summan
+                }
+                else if (dataGridView1.Rows[rivi].Cells[0].Value != null && TuomariPisteet.Count < 3)   // jos alle 3 tuomari ilmoittaa virhe ilmoituksen
+                {
+                    MessageBox.Show("tarvitaan vähintään 3 tuomaria");
+                    break;
+                }
+                KokonaisPisteet = tyyliPisteet + hypynPituuspisteet; // laskee tuomari ja hyppypisteet yhteen
+
+                dataGridView1.Rows[rivi].Cells[7].Value = hypynPituuspisteet.ToString();    //hypynpituus pisteet
+                dataGridView1.Rows[rivi].Cells[8].Value = tyyliPisteet.ToString();          //Tuomarin pisteet
+                dataGridView1.Rows[rivi].Cells[9].Value = KokonaisPisteet.ToString();       //Kokonaispisteet pisteet
+                Console.WriteLine(Kpiste.ToString()+" " + hypynPituus.ToString()+" "  + hypynPituuspisteet.ToString()); // debugging printtaa piste arvot
+            }  
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
